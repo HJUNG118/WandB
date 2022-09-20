e8e9a17d3f4b2422755718a06b14e52d8e998a32
HJUNG118 <a38722243@gmail.com>
2022-09-20 10:44:43 +0900

    loadcell & sw assembly complete

--- keypad_modify/keypad_modify.ino
+++ keypad_modify/keypad_modify.ino
@@ -1,42 +1,35 @@
-int sw[12] = {41, 39, 37, 35, 33, 31, 28, 26, 24, 22, 20, 18};
-int led = 2;
-
+#include "wandb.h"
+int sw[12] = {27, 26, 25, 24, 23, 22, 8, 9, 10, 11, 12, 13};
 
 void setup() {
   Serial.begin(9600);
-  
-  for(int i = 0; i < 48; i++)
-  {
-    pinMode(sw[i], INPUT_PULLUP);
-  }
-  pinMode(led, OUTPUT);
-
-
+  SWinput(sw, 12);
 }
 
 void loop() {
 
   int SWselect[12];
-
-  for(int i = 0; i < 12; i++)
-  {
-    SWselect[i] = digitalRead(sw[i]);
-  }
-
+  SWread(SWselect, sw, 12);
+   //int grn = 1;
+   //int yew = 2;
+   //int red = 3;
 
   if(!SWselect[0] && !SWselect[1] && !SWselect[2] && !SWselect[3] && !SWselect[4] && !SWselect[5]
   && !SWselect[6] && !SWselect[7] && !SWselect[8] && !SWselect[9] && !SWselect[10] && !SWselect[11])
   {
-    digitalWrite(led, 1);
+    Serial.println('r');
+    delay(1000);
   }
   else if(SWselect[0] && SWselect[1] && SWselect[2] && SWselect[3] && SWselect[4] && SWselect[5]
   && SWselect[6] && SWselect[7] && SWselect[8] && SWselect[9] && SWselect[10] && SWselect[11])
   {
-    digitalWrite(led, 0);
+    Serial.println('g');
+    delay(1000);
   }
   else
   {
-    Serial.println("yellow");
+    Serial.println('y');
+    delay(1000);
   }
 
 }
